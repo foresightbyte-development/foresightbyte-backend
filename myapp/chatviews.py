6f2eafcbcef4c1d5554732fb9098b57777d06855
Md. Asadozzaman <83003038+asadozzaman@users.noreply.github.com>
2024-10-13 21:26:21 +0600

    fix errors

--- myapp/chatviews.py
+++ myapp/chatviews.py
@@ -21,7 +21,7 @@ def chatui(request):
 
 def chat_add(request):
     offset = request.GET.get('offset')
-    response = requests.post('http://localhost:8000/ask/', json={"question_text": offset})
+    response = requests.post('https://foresightbyte.onrender.com/ask/', json={"question_text": offset})
     data = response.json()  # Convert the response to JSON
 
     if 'counter' not in request.session:
